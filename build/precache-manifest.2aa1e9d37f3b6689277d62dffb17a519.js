af948f041df7e17c6d1ae459294dd65a11ce9373
ZohrehTaheri <79049757+ZohrehTaheri@users.noreply.github.com>
2021-04-03 00:00:06 +0430

    add files

---
+++ build/precache-manifest.2aa1e9d37f3b6689277d62dffb17a519.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ce2e1ee280f4b0a9e90f",
+    "url": "/bnk48-face-recognition/static/css/main.3c4e75ff.chunk.css"
+  },
+  {
+    "revision": "ce2e1ee280f4b0a9e90f",
+    "url": "/bnk48-face-recognition/static/js/main.ce2e1ee2.chunk.js"
+  },
+  {
+    "revision": "776535289e2aa58c4523",
+    "url": "/bnk48-face-recognition/static/js/1.77653528.chunk.js"
+  },
+  {
+    "revision": "4942cd80093195c08172",
+    "url": "/bnk48-face-recognition/static/js/runtime~main.4942cd80.js"
+  },
+  {
+    "revision": "34e86f06102f347f0475d68f8673318b",
+    "url": "/bnk48-face-recognition/static/media/test.34e86f06.jpg"
+  },
+  {
+    "revision": "cc46e63d574915a8430eed2a3f4fe165",
+    "url": "/bnk48-face-recognition/static/media/example.cc46e63d.jpg"
+  },
+  {
+    "revision": "1d0b65b5a88393eabb59ffc81cc5080e",
+    "url": "/bnk48-face-recognition/index.html"
+  }
+];
